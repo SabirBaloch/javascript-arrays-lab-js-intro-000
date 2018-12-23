6a13a0aeb543a8692831e902da66bfe5bfa6d8a5
Sabir Baloch <baloch_jan01@live.com>
2018-12-23 18:17:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ['Milo','Otis','Garfield']; //define your array here
 
 // Add your functions and code here
 var destructivelyAppendKitten=(name)=>{
-kittens.append('Ralph');
+$(kittens).append('Ralph');
 return kittens;
 }
