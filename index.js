dad20ba16d9ac5c5af1e4f21e3b1079e106d17b4
Sabir Baloch <baloch_jan01@live.com>
2018-12-23 18:49:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ var kittens = ['Milo','Otis','Garfield']; //define your array here
 // Add your functions and code here
 var destructivelyAppendKitten=(name)=>{
 kittens.push('Ralph');
+return kittens;
 
 }
 var destructivelyPrependKitten=(name)=>{
 kittens.unshift('Bob');
+return kittens;
 }
 var destructivelyRemoveLastKitten=()=>{
 kittens.pop();
+return kittens;
 
 }
 var destructivelyRemoveFirstKitten=()=>{
   kittens.shift();
+  return kittens;
 }
 var appendKitten=(name)=>{
   return [...kittens, 'Broom'];
